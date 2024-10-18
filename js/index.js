6fde56975e4733384580ae2178c9e1a8d0d07b89
Masato Nishikawa <ballondors83@gmail.com>
2024-10-18 18:23:24 +0900

    header作成

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// ファーストビューを超えたらヘッダーが出現
+const secondSection = document.querySelector("[data-second-section]");
+const followingHeader = document.querySelector("[data-following-header]");
+
+window.addEventListener("scroll", () => {
+  const sectionRect = secondSection.getBoundingClientRect();
+
+  if (sectionRect.top <= 0) {
+    followingHeader.classList.add("is-active");
+  } else {
+    followingHeader.classList.remove("is-active");
+  }
+});
