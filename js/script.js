d289f48e758ff2f3148ce8e5f14d340afc94d557
Masato Nishikawa <ballondors83@gmail.com>
2024-10-19 16:30:47 +0900

    グローバルメニュー作成

--- js/script.js
+++ js/script.js
@@ -15,3 +15,38 @@
   addEventListener("resize", switchViewport, false);
   switchViewport();
 })();
+
+// グローバルメニュー
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButtons = document.querySelectorAll(".js-hamburger-button");
+  const globalMenu = document.getElementById("js-global-menu");
+  const globalMenuOverlay = document.getElementById("js-global-menu-overlay");
+  const body = document.body;
+
+  // ハンバーガーボタンクリック時
+  hamburgerButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      body.classList.toggle("is-drawerActive");
+      const isExpanded = body.classList.contains("is-drawerActive");
+
+      hamburgerButtons.forEach((button) => {
+        button.setAttribute("aria-expanded", isExpanded.toString());
+      });
+
+      if (isExpanded) {
+        globalMenu.setAttribute("aria-hidden", "false");
+      } else {
+        globalMenu.setAttribute("aria-hidden", "true");
+      }
+    });
+  });
+
+  // 背景クリック時
+  globalMenuOverlay.addEventListener("click", () => {
+    body.classList.remove("is-drawerActive");
+    hamburgerButtons.forEach((button) => {
+      button.setAttribute("aria-expanded", "false");
+    });
+    globalMenu.setAttribute("aria-hidden", "true");
+  });
+});
