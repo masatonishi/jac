1b5a80643c3f608892a33f9f8ba1842bfac31e7c
Masato Nishikawa <ballondors83@gmail.com>
2024-10-22 11:17:14 +0900

    トップページ：マスクアニメーション追加

--- js/script.js
+++ js/script.js
@@ -138,3 +138,9 @@ if (urlHash) {
     });
   }
 }
+
+// DOMが読み込まれたらbodyにクラス追加
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  body.classList.add("is-loaded");
+});
